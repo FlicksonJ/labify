982603a4c15def8d31b5c58a28ed307f26ccc7ef
h4r1337 <72344536+h4r1337@users.noreply.github.com>
2024-04-07 00:02:57 +0530

    Finish Create account page

--- app/ui/ui_main.py
+++ app/ui/ui_main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'untitledXTBuEu.ui'
+## Form generated from reading UI file 'untitled.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.6.2
 ##
@@ -121,13 +121,54 @@ class Ui_MainWindow(object):
 "	margin: 0;\n"
 "}\n"
 "\n"
+"#create_user_page {\n"
+"	background: #fff;\n"
+"}\n"
 "\n"
-"")
+"#create_user_form QLabel {\n"
+"	color: rgb(52, 62, 162);\n"
+"	font-size: 15pt;\n"
+"	max-height: 20px;\n"
+"	margin-top: 10px;\n"
+"	padding-top:30px\n"
+"}\n"
+"\n"
+"#create_user_form QLineEdit {\n"
+"	border: 1px solid;\n"
+"	min-height: 80px;\n"
+"	border-radius: 5px;\n"
+"	border-color: rgb(52, 62, 162);\n"
+"	color: black;\n"
+"	padding-left: 10px;\n"
+"	padding-right: 10px;\n"
+"}\n"
+"\n"
+"#"
+                        "create_account_button {\n"
+"	background: rgb(0, 159, 161);\n"
+"	font-size: 25pt;\n"
+"	margin: 0;\n"
+"	margin-right: 40px;\n"
+"}\n"
+"\n"
+"#cancel_button {\n"
+"	border: 1px solid;\n"
+"	border-color: rgb(224, 27, 36);\n"
+"	color: rgb(224, 27, 36);\n"
+"	font-size: 25pt;\n"
+"	margin: 0;\n"
+"	padding: 0px 50px 0px 50px\n"
+"}\n"
+"\n"
+"#home_page {\n"
+"	background: #fff;\n"
+"}")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.stackedWidget = QStackedWidget(self.centralwidget)
         self.stackedWidget.setObjectName(u"stackedWidget")
         self.stackedWidget.setGeometry(QRect(0, 0, 1600, 900))
+        self.stackedWidget.setStyleSheet(u"")
         self.login_page = QWidget()
         self.login_page.setObjectName(u"login_page")
         self.verticalLayoutWidget = QWidget(self.login_page)
@@ -173,13 +214,14 @@ class Ui_MainWindow(object):
         self.stackedWidget.addWidget(self.login_page)
         self.home_page = QWidget()
         self.home_page.setObjectName(u"home_page")
-        self.widget = QWidget(self.home_page)
-        self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(0, 0, 1602, 899))
-        self.verticalLayout_2 = QVBoxLayout(self.widget)
+        self.home_page.setStyleSheet(u"")
+        self.layoutWidget = QWidget(self.home_page)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(0, 0, 1602, 899))
+        self.verticalLayout_2 = QVBoxLayout(self.layoutWidget)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.header = QWidget(self.widget)
+        self.header = QWidget(self.layoutWidget)
         self.header.setObjectName(u"header")
         self.header.setMinimumSize(QSize(1600, 100))
         self.header.setMaximumSize(QSize(1600, 120))
@@ -280,20 +322,114 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_2.addWidget(self.header)
 
-        self.stackedWidget_2 = QStackedWidget(self.widget)
+        self.stackedWidget_2 = QStackedWidget(self.layoutWidget)
         self.stackedWidget_2.setObjectName(u"stackedWidget_2")
         self.stackedWidget_2.setMinimumSize(QSize(3, 0))
-        self.page = QWidget()
-        self.page.setObjectName(u"page")
-        self.stackedWidget_2.addWidget(self.page)
+        self.create_user_page = QWidget()
+        self.create_user_page.setObjectName(u"create_user_page")
+        self.widget = QWidget(self.create_user_page)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(0, -10, 1601, 751))
+        self.horizontalLayout_3 = QHBoxLayout(self.widget)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.widget1 = QWidget(self.widget)
+        self.widget1.setObjectName(u"widget1")
+        sizePolicy4 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        sizePolicy4.setHorizontalStretch(0)
+        sizePolicy4.setVerticalStretch(3)
+        sizePolicy4.setHeightForWidth(self.widget1.sizePolicy().hasHeightForWidth())
+        self.widget1.setSizePolicy(sizePolicy4)
+        self.widget1.setStyleSheet(u"")
+        self.label_2 = QLabel(self.widget1)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(50, -50, 670, 800))
+        self.label_2.setLineWidth(0)
+        self.label_2.setPixmap(QPixmap(u":/images/images/create_user.jpeg"))
+        self.label_2.setScaledContents(True)
+
+        self.horizontalLayout_3.addWidget(self.widget1)
+
+        self.create_user_form = QWidget(self.widget)
+        self.create_user_form.setObjectName(u"create_user_form")
+        sizePolicy4.setHeightForWidth(self.create_user_form.sizePolicy().hasHeightForWidth())
+        self.create_user_form.setSizePolicy(sizePolicy4)
+        self.verticalLayoutWidget_2 = QWidget(self.create_user_form)
+        self.verticalLayoutWidget_2.setObjectName(u"verticalLayoutWidget_2")
+        self.verticalLayoutWidget_2.setGeometry(QRect(80, 70, 561, 611))
+        self.verticalLayout_3 = QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.cu_username_label = QLabel(self.verticalLayoutWidget_2)
+        self.cu_username_label.setObjectName(u"cu_username_label")
+
+        self.verticalLayout_3.addWidget(self.cu_username_label)
+
+        self.cu_username_input = QLineEdit(self.verticalLayoutWidget_2)
+        self.cu_username_input.setObjectName(u"cu_username_input")
+        font3 = QFont()
+        font3.setPointSize(21)
+        self.cu_username_input.setFont(font3)
+        self.cu_username_input.setFrame(True)
+
+        self.verticalLayout_3.addWidget(self.cu_username_input)
+
+        self.cu_password_label = QLabel(self.verticalLayoutWidget_2)
+        self.cu_password_label.setObjectName(u"cu_password_label")
+
+        self.verticalLayout_3.addWidget(self.cu_password_label)
+
+        self.cu_password_input = QLineEdit(self.verticalLayoutWidget_2)
+        self.cu_password_input.setObjectName(u"cu_password_input")
+        self.cu_password_input.setFont(font3)
+        self.cu_password_input.setEchoMode(QLineEdit.Password)
+
+        self.verticalLayout_3.addWidget(self.cu_password_input)
+
+        self.cu_confirm_password_label = QLabel(self.verticalLayoutWidget_2)
+        self.cu_confirm_password_label.setObjectName(u"cu_confirm_password_label")
+
+        self.verticalLayout_3.addWidget(self.cu_confirm_password_label)
+
+        self.cu_confirm_password_input = QLineEdit(self.verticalLayoutWidget_2)
+        self.cu_confirm_password_input.setObjectName(u"cu_confirm_password_input")
+        self.cu_confirm_password_input.setFont(font3)
+        self.cu_confirm_password_input.setEchoMode(QLineEdit.Password)
+
+        self.verticalLayout_3.addWidget(self.cu_confirm_password_input)
+
+        self.horizontalLayout_6 = QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.create_account_button = QPushButton(self.verticalLayoutWidget_2)
+        self.create_account_button.setObjectName(u"create_account_button")
+        self.create_account_button.setCursor(QCursor(Qt.PointingHandCursor))
+
+        self.horizontalLayout_6.addWidget(self.create_account_button)
+
+        self.cancel_button = QPushButton(self.verticalLayoutWidget_2)
+        self.cancel_button.setObjectName(u"cancel_button")
+        sizePolicy.setHeightForWidth(self.cancel_button.sizePolicy().hasHeightForWidth())
+        self.cancel_button.setSizePolicy(sizePolicy)
+        self.cancel_button.setCursor(QCursor(Qt.PointingHandCursor))
+
+        self.horizontalLayout_6.addWidget(self.cancel_button)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_6)
+
+
+        self.horizontalLayout_3.addWidget(self.create_user_form)
+
+        self.stackedWidget_2.addWidget(self.create_user_page)
         self.page_2 = QWidget()
         self.page_2.setObjectName(u"page_2")
         self.stackedWidget_2.addWidget(self.page_2)
 
         self.verticalLayout_2.addWidget(self.stackedWidget_2)
 
-        self.footer = QWidget(self.widget)
+        self.footer = QWidget(self.layoutWidget)
         self.footer.setObjectName(u"footer")
+        self.footer.setStyleSheet(u"")
         self.horizontalLayout_5 = QHBoxLayout(self.footer)
         self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
         self.horizontalLayout_5.setContentsMargins(0, 0, 0, 0)
@@ -301,6 +437,8 @@ class Ui_MainWindow(object):
         self.label.setObjectName(u"label")
         self.label.setMinimumSize(QSize(0, 30))
         self.label.setFont(font1)
+        self.label.setAutoFillBackground(False)
+        self.label.setLineWidth(0)
         self.label.setTextFormat(Qt.RichText)
         self.label.setAlignment(Qt.AlignCenter)
 
@@ -313,6 +451,10 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
+
+        self.stackedWidget.setCurrentIndex(1)
+        self.cancel_button.setDefault(False)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
@@ -328,6 +470,12 @@ class Ui_MainWindow(object):
         self.header_username_label.setText(QCoreApplication.translate("MainWindow", u"ADMIN", None))
         self.create_user_button.setText(QCoreApplication.translate("MainWindow", u"Create User", None))
         self.logout_button.setText(QCoreApplication.translate("MainWindow", u"Logout", None))
+        self.label_2.setText("")
+        self.cu_username_label.setText(QCoreApplication.translate("MainWindow", u"Username", None))
+        self.cu_password_label.setText(QCoreApplication.translate("MainWindow", u"Password", None))
+        self.cu_confirm_password_label.setText(QCoreApplication.translate("MainWindow", u"Confirm Password", None))
+        self.create_account_button.setText(QCoreApplication.translate("MainWindow", u"CREATE ACCOUNT", None))
+        self.cancel_button.setText(QCoreApplication.translate("MainWindow", u"CANCEL", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"&copy; 2024, Dept. of CS", None))
     # retranslateUi
 

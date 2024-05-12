fd5a6ff98b0fc7f78ef2b4abef7f01d08191b57f
h4r1337 <72344536+h4r1337@users.noreply.github.com>
2024-05-12 22:02:18 +0530

    Add application name

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QCoreApplication
 
 from app.main_window import MainWindow
 
 if __name__ == "__main__":
     app = QApplication([])
+    QCoreApplication.setApplicationName("Labify")
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
